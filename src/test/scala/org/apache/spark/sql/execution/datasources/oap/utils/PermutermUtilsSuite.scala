53796d8ae292ece50c8b27d11b95523afa4ce2bc
Daoyuan Wang <daoyuan.wang@intel.com>
2017-10-29 23:46:47 +0800

    fix test

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/utils/PermutermUtilsSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/utils/PermutermUtilsSuite.scala
@@ -20,14 +20,15 @@ package org.apache.spark.sql.execution.datasources.oap.index
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.execution.datasources.oap.utils.PermutermUtils
+import org.apache.spark.unsafe.types.UTF8String
 
 class PermutermUtilsSuite extends SparkFunSuite {
   test("generate permuterm") {
-    val row1 = InternalRow("Alpha")
-    val row2 = InternalRow("Alphabeta")
-    val row3 = InternalRow("AlphaHello")
-    val row4 = InternalRow("Beta")
-    val row5 = InternalRow("Zero")
+    val row1 = InternalRow(UTF8String.fromString("Alpha"))
+    val row2 = InternalRow(UTF8String.fromString("Alphabeta"))
+    val row3 = InternalRow(UTF8String.fromString("AlphaHello"))
+    val row4 = InternalRow(UTF8String.fromString("Beta"))
+    val row5 = InternalRow(UTF8String.fromString("Zero"))
 
     val uniqueList = new java.util.LinkedList[InternalRow]()
     val offsetMap = new java.util.HashMap[InternalRow, Int]()
@@ -35,16 +36,72 @@ class PermutermUtilsSuite extends SparkFunSuite {
     val list1 = List(row1, row2)
     list1.foreach(uniqueList.add)
     list1.zipWithIndex.foreach(i => offsetMap.put(i._1, i._2))
-    val trie1 = PermutermUtils.generatePermuterm(uniqueList, offsetMap)
-    assert(trie1.toString.equals(""))
+    val trie1 = InMemoryTrie()
+    val trieLength1 = PermutermUtils.generatePermuterm(uniqueList, offsetMap, trie1)
+    assert(trieLength1 == 109)
+    assert(trie1.toString.equals(
+      "[Trie(\\x0,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,0) " +
+        "[Trie(b,-1) [Trie(e,-1) [Trie(t,-1) Trie(a,1)]]]]]]]]] [Trie(A,-1) [Trie(l,-1) [Trie" +
+        "(p,-1) [Trie(h,-1) [Trie(a,-1) Trie($,0) [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a," +
+        "-1) Trie($,1)]]]]]]]]] [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [" +
+        "Trie(h,0) [Trie(a,-1) [Trie(b,-1) [Trie(e,-1) Trie(t,1)]]]]]]]] [Trie(b,-1) [Trie(e," +
+        "-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) Trie(h,1)" +
+        "]]]]]]]]] [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [" +
+        "Trie(l,-1) [Trie(p,-1) [Trie(h,-1) Trie(a,1)]]]]]]]]] [Trie(e,-1) [Trie(t,-1) [Trie(a," +
+        "-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) Trie(b,1)" +
+        "]]]]]]]]] [Trie(h,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) Trie(p,0)]]] [" +
+        "Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) " +
+        "Trie(p,1)]]]]]]]]] [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [Trie($,-1) Trie" +
+        "(A,0)] [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) Trie(A,1)]]]]]]]]]" +
+        " [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) Trie(l,0)]] [Trie(b,-1) [" +
+        "Trie(e,-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) Trie(l,1)]]]]]]]]] [Trie(" +
+        "t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a," +
+        "-1) [Trie(b,-1) Trie(e,1)]]]]]]]]]]"))
     uniqueList.clear()
     offsetMap.clear()
 
     val list2 = List(row2, row3, row4, row5)
     list2.foreach(uniqueList.add)
     list2.zipWithIndex.foreach(i => offsetMap.put(i._1, i._2))
-    val trie2 = PermutermUtils.generatePermuterm(uniqueList, offsetMap)
-    assert(trie2.toString.equals(""))
+    val trie2 = InMemoryTrie()
+    val trieLength2 = PermutermUtils.generatePermuterm(uniqueList, offsetMap, trie2)
+    assert(trieLength2 == 232)
+    assert(trie2.toString.equals(
+      "[Trie(\\x0,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1)" +
+        " [Trie(H,-1) [Trie(e,-1) [Trie(l,-1) [Trie(l,-1) Trie(o,1)]]]] [Trie(b,-1) [Trie(e,-1)" +
+        " [Trie(t,-1) Trie(a,0)]]]]]]]] [Trie(B,-1) [Trie(e,-1) [Trie(t,-1) Trie(a,2)]]] [Trie(" +
+        "Z,-1) [Trie(e,-1) [Trie(r,-1) Trie(o,3)]]]] [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(" +
+        "h,-1) [Trie(a,-1) [Trie(H,-1) [Trie(e,-1) [Trie(l,-1) [Trie(l,-1) [Trie(o,-1) Trie($,1" +
+        ")]]]]] [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) Trie($,0)]]]]]]]]] [Trie(B,-1)" +
+        " [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) Trie($,2)]]]] [Trie(H,-1) [Trie(e,-1) [Trie(l,-1)" +
+        " [Trie(l,-1) [Trie(o,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) " +
+        "Trie(a,1)]]]]]]]]]] [Trie(Z,-1) [Trie(e,-1) [Trie(r,-1) [Trie(o,-1) Trie($,3)]]]] [" +
+        "Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [" +
+        "Trie(b,-1) [Trie(e,-1) Trie(t,0)]]]]]]] [Trie(B,-1) [Trie(e,-1) Trie(t,2)]]] [Trie(H," +
+        "-1) [Trie(e,-1) [Trie(l,-1) [Trie(l,-1) [Trie(o,-1) [Trie($,-1) [Trie(A,-1) [Trie(l," +
+        "-1) [Trie(p,-1) Trie(h,1)]]]]]]]]] [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) [" +
+        "Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) Trie(h,0)]]]]]]]]] [Trie(b,-1) [Trie(e," +
+        "-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h," +
+        "-1) Trie(a,0)]]]]]]]]] [Trie(e,-1) [Trie(l,-1) [Trie(l,-1) [Trie(o,-1) [Trie($,-1) [" +
+        "Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) Trie(H,1)]]]]]]]]] [Trie(" +
+        "r,-1) [Trie(o,-1) [Trie($,-1) Trie(Z,3)]]] [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(" +
+        "A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) Trie(b,0)]]]]] Trie(B,2)]]]] [" +
+        "Trie(h,-1) [Trie(a,-1) [Trie(H,-1) [Trie(e,-1) [Trie(l,-1) [Trie(l,-1) [Trie(o,-1) [" +
+        "Trie($,-1) [Trie(A,-1) [Trie(l,-1) Trie(p,1)]]]]]]]] [Trie(b,-1) [Trie(e,-1) [Trie(t," +
+        "-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) Trie(p,0)]]]]]]]]] [Trie(l,-1) [" +
+        "Trie(l,-1) [Trie(o,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [" +
+        "Trie(a,-1) [Trie(H,-1) Trie(e,1)]]]]]]]]] [Trie(o,-1) [Trie($,-1) [Trie(A,-1) [Trie(" +
+        "l,-1) [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [Trie(H,-1) [Trie(e,-1) Trie(l,1)]]]]]]]]]" +
+        " [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [Trie(H,-1) [Trie(e,-1) [Trie(l,-1) [Trie(l,-1)" +
+        " [Trie(o,-1) [Trie($,-1) Trie(A,1)]]]]]] [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a," +
+        "-1) [Trie($,-1) Trie(A,0)]]]]]]]]] [Trie(o,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [" +
+        "Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [Trie(H,-1) [Trie(e,-1) [Trie(l,-1) Trie(l,1)]]]]" +
+        "]]]] [Trie(Z,-1) [Trie(e,-1) Trie(r,3)]]]] [Trie(p,-1) [Trie(h,-1) [Trie(a,-1) [Trie(" +
+        "H,-1) [Trie(e,-1) [Trie(l,-1) [Trie(l,-1) [Trie(o,-1) [Trie($,-1) [Trie(A,-1) Trie(l," +
+        "1)]]]]]]] [Trie(b,-1) [Trie(e,-1) [Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) " +
+        "Trie(l,0)]]]]]]]]] [Trie(r,-1) [Trie(o,-1) [Trie($,-1) [Trie(Z,-1) Trie(e,3)]]]] [" +
+        "Trie(t,-1) [Trie(a,-1) [Trie($,-1) [Trie(A,-1) [Trie(l,-1) [Trie(p,-1) [Trie(h,-1) [" +
+        "Trie(a,-1) [Trie(b,-1) Trie(e,0)]]]]]] [Trie(B,-1) Trie(e,2)]]]]]"))
     uniqueList.clear()
     offsetMap.clear()
   }
